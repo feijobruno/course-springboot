f5368da199793be1c694454e737f478bafbf5c89
Bruno Feijó Rocha <bruno.fsilva@al.infnet.edu.br>
2019-09-23 16:01:26 -0300

    User entity and resource

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.example.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Bruno Feijo", "bruno.feijo@live.com", "21988343888", "030201");
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
